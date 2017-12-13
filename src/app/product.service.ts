37f889fe2c65717fed25e67e0920030860ee6083
Drazes <drazes_@hotmail.com>
2017-12-12 19:41:42 -0800

    minor fix for course

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -18,7 +18,7 @@ export class ProductService {
   }
 
   public getProducts(): Observable<Product[]> {
-    return this._http.get(this._productsUrl).map(response => 
+    return this._http.get(this._productsUrl).map(response =>
       <Product[]>response.json());
   }
 }
