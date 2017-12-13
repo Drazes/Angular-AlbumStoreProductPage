e3918772d467634c389695871e42e5167c9c5f8e
Drazes <drazes_@hotmail.com>
2017-12-12 19:09:06 -0800

    Adding type Album and Track

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+
+import { Album } from './album';
 
 @Injectable()
 export class ProductService {
@@ -8,7 +11,7 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  public getAlbum(id: number) {
-      return this._http.get(this._albumUrl).map((response) => response.json());
+  public getAlbum(id: number): Observable<Album> {
+      return this._http.get(this._albumUrl).map((response) => <Album>response.json());
   }
 }
